c8f2177fef58bcdb0284dd04f7cffda280897cca
Jannik Reinhard <73911860+JayRHa@users.noreply.github.com>
2026-07-26 16:31:40 +0200

    Fix tray startup and resilient system information (#3)
    * fix tray startup and system info
    
    * refresh Intune deployment package
    
    ---------
    
    Co-authored-by: Jannik Reinhard <jannik.reinhard@epicfusion.com>

--- sources/install.bat
+++ sources/install.bat
@@ -1,2 +1,8 @@
-powershell.exe -executionpolicy bypass -command "& '.\Install-IntuneSystemTrayV2.ps1'"
-schtasks /Create /XML StartCompanyPortalSystemtray.xml /TN StartCompanyPortalSystemtray
+@echo off
+setlocal
+
+powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0Install-IntuneSystemTrayV2.ps1"
+if errorlevel 1 exit /b %errorlevel%
+
+schtasks.exe /Create /XML "%~dp0StartCompanyPortalSystemtray-v2.xml" /TN "StartCompanyPortalSystemtray" /F
+exit /b %errorlevel%
